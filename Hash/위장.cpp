d15e14616d9dd2326935517f7ef78cf265d0d0b9
이준규 <jkjkjk5710@gmail.com>
2021-03-08 11:30:11 +0900

    거의수학

---
+++ Hash/위장.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes)
+{
+    int answer = 0;
+    unordered_map<string, vector<string>> m;
+
+    for (int i = 0; i < clothes.size(); i++)
+    {
+        m[clothes[i][1]].push_back(clothes[i][0]);
+    }
+
+    int a = 1;
+
+    for (pair<string, vector<string>> i : m)
+    {
+        a *= (i.second.size() + 1);
+    }
+
+    return answer + a - 1;
+}
