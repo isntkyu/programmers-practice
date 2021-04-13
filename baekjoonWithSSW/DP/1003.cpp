9a0f4ebd6ea4734c441be45b835254cdea810bfd
이준규 <jkjkjk5710@gmail.com>
2021-04-13 13:36:12 +0900

    DP 시작

---
+++ baekjoonWithSSW/DP/1003.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+pair<int, int> arr[41];
+
+void pivo(int n)
+{
+    if (n == 0)
+    {
+        arr[n].first = 1;
+        return;
+    }
+    else if (n == 1)
+    {
+        arr[n].second = 1;
+        return;
+    }
+    else
+    {
+        if (arr[n - 2].first == 0 || arr[n - 2].second == 0)
+            pivo(n - 2);
+        if (arr[n - 1].first == 0 || arr[n - 1].second == 0)
+            pivo(n - 1);
+
+        arr[n].first = arr[n - 2].first + arr[n - 1].first;
+        arr[n].second = arr[n - 2].second + arr[n - 1].second;
+
+        return;
+    }
+}
+
+int main()
+{
+    int n, t;
+
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> n;
+
+        pivo(n);
+
+        cout << arr[n].first << " " << arr[n].second << "\n";
+    }
+}
