9776b03eb9744233185b3fad6fab3966a9a515d1
이준규 <jkjkjk5710@gmail.com>
2022-03-04 23:18:03 +0900

    레베르3

---
+++ level3/야근지수.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+long long solution(int n, vector<int> works) {
+
+    long long answer = 0;
+
+    priority_queue<int> pq;
+
+    for(int i = 0; i < works.size(); i++){
+        pq.push(works[i]);
+    }
+
+    while(n--){
+        if(pq.top() > 0){
+
+            pq.push(pq.top() - 1);
+            pq.pop();
+
+        }
+    }
+
+    while(!pq.empty()){
+        answer += pq.top() * pq.top();
+        pq.pop();
+    }
+    
+    return answer;
+}
